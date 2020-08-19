9bb0d5f5b53311e548b320c19bfac2c34005f80f
Prudhvi Raj Madhu <prudhvi17madhu@gmail.com>
2020-08-19 17:01:28 +0530

    Gradle commit

---
+++ src/main/java/com/jenkins/controller/Jenkins.java
@@ -0,0 +1,15 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class Jenkins {
+
+    @GetMapping
+    public String message(){
+        return "Before Update";
+    }
+}
