d77745fabe76d08021f79d413c9a16d22de45051
Prudhvi Raj Madhu <prudhvi17madhu@gmail.com>
2020-08-20 13:44:20 +0530

    jenkinsFile

--- src/main/java/com/jenkins/controller/Jenkins.java
+++ src/main/java/com/jenkins/controller/Jenkins.java
@@ -10,6 +10,6 @@ public class Jenkins {
 
     @GetMapping
     public String message(){
-        return "After configuring stop cluster the Update";
+        return "After configuring jenkins file";
     }
 }
